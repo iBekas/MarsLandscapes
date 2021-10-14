37c170e21667c1d22e4fc406caad3b422380319a
iBekas <dmitrbekasov@gmail.com>
2021-10-15 01:49:52 +0300

    Подготовка макета для "фото дня": три чипа над фото, само фото, поиск в вики. Добавлен BottomAppBar. Вдохновлялся\заимствовал примером с лекции.

--- app/src/main/java/space/picture/marslandscapes/view/MainActivity.kt
+++ app/src/main/java/space/picture/marslandscapes/view/MainActivity.kt
@@ -3,10 +3,19 @@ package space.picture.marslandscapes.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import space.picture.marslandscapes.R
+import space.picture.marslandscapes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        savedInstanceState.let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        }
     }
 }
